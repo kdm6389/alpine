f23c4d407dfe7aaa7fc4492da76f4df4be1cd7ae
Kapil Dev Mishra <kdm6389@gmail.com>
2024-10-07 23:49:48 +0530

    Update and rename answer.txt to answer.sh

--- answer.sh
+++ answer.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 USE_EFI=1 BOOT_SIZE=32 BOOTSIZE=32 BOOTFS=vfat SWAP_SIZE=0 DISKLABEL=gpt ROOTFS=ext4 KEYMAPOPTS="us us" TIMEZONEOPTS="-z Asia/Kolkata" PROXYOPTS="none" SSHDOPTS="-c openssh" NTPOPTS="-c chrony" HOSTNAMEOPTS="-n raspi-alp"
